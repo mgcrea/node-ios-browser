3539c2d4aba0003ecd10ac752f85548c920c9207
Olivier Louvignes <olivier@mg-crea.com>
2013-05-15 16:44:39 +0200

    chore(2.7.0): tweaks for cordova 2.7.0

--- src/main.m
+++ src/main.m
@@ -18,6 +18,10 @@
  */
 //
 //  main.m
+//  __TESTING__
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
